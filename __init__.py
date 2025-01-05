2622a6229952e8f4d7e8d10114dbb80987236362
rxnk <o7996784@gmail.com>
2025-01-04 22:31:11 -0600

    bb

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from typing import Literal
 from aiohttp import ClientSession
 from .routes import Instagram
 
+
 class SharedAPI:
     """A high performance API for retrieving Social Media data."""
 
@@ -12,25 +13,25 @@ class SharedAPI:
     def __init__(self, api_key: str) -> None:
         self.api_key = api_key
         self.session = ClientSession(
-            base_url="https://shared.egirl.software",
-            headers={"Authorization": api_key}
+            base_url="https://shared.egirl.software", headers={"Authorization": api_key}
         )
         self.instagram = Instagram(self)
-    
+
     async def request(self, method: Literal["GET", "POST"], endpoint: str, **kwargs):
         """Submit a request to the API."""
-        
+
         async with self.session.request(method, endpoint, **kwargs) as response:
-            data = await response.json()
+            try:
+                data = await response.json()
+            except Exception as exc:
+                raise ValueError("The API returned an invalid response") from exc
+
             if "error" in data:
                 raise ValueError(data["error"])
-            
-            elif not response.ok:
-                raise ValueError("The shared API raised an exception.")
-            
+
             return data
-        
+
     async def close(self) -> None:
         """Close the API session."""
-        
+
         await self.session.close()
